750864e02e02bfe8c2ba3e5ec78f58d181fc51a5
Thet Hnin <thethnin@momnpop.asia>
2023-12-01 15:44:31 +0630

    [add gradle file]

--- lib/ui/home/home_widget.dart
+++ lib/ui/home/home_widget.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:ithub_quiz/ui/auth/auth_firebase.dart';
 
 class HomeWidget extends StatefulWidget {
   const HomeWidget({super.key});
@@ -8,6 +10,24 @@ class HomeWidget extends StatefulWidget {
 }
 
 class _HomeWidgetState extends State<HomeWidget> {
+  final User? user = Auth().currenUser;
+
+  Future<void> signOut() async {
+    await Auth().signOut();
+  }
+
+  Widget title() {
+    return const Text("Firebase Auth");
+  }
+
+  Widget userUid() {
+    return Text(user?.email ?? "Firebase Auth");
+  }
+
+  Widget signOutButton() {
+    return ElevatedButton(onPressed: signOut, child: const Text("Sign Out"));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
