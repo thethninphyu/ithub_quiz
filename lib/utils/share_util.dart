64f6482f257373be77bd52317dee1f1379ba8902
Thet Hnin <thethnin@momnpop.asia>
2023-12-26 14:03:18 +0630

    [add share preferences ]

--- lib/utils/share_util.dart
+++ lib/utils/share_util.dart
@@ -1,4 +1,13 @@
-class StoreUserData {
-//  SharedPreferences prefs = await SharedPreferences.getInstance();
+import 'package:shared_preferences/shared_preferences.dart';
 
+class StoreUserData {
+  setRole(String role) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString("ROLE", role);
+  }
+
+  getRole() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.getString("ROLE");
+  }
 }
