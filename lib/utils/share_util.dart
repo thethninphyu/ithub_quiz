40749356af1a75ebe3729ed073fb7f9efc185595
pan myintmo <john.doe@example.com>
2023-12-26 13:42:44 +0630

    [sharedPreference]

--- lib/utils/share_util.dart
+++ lib/utils/share_util.dart
@@ -1,8 +1,4 @@
 class StoreUserData {
 //  SharedPreferences prefs = await SharedPreferences.getInstance();
 
-  Future<void> setAuthToken(String token) async {
-    final SharedPreferences preferences = await SharedPreferences.getInstance();
-    await preferences.setString(tokenKey, token);
-  }
 }
