0c1897862fd4ea5b9ad3dccf0dcc4aafe3e69d5f
pan myintmo <john.doe@example.com>
2024-01-18 15:28:59 +0630

    [conflict part]

--- lib/ui/admin_screen/quiz_ans_screen.dart
+++ lib/ui/admin_screen/quiz_ans_screen.dart
@@ -50,8 +50,10 @@ class _QuizAnswerScreenState extends State<QuizAnswerScreen> {
             setState(() {
               questionAndAnswer = snapshot.data()!['questionAndAnswer'];
 
-              logger.e('Que and Ans $questionAndAnswer');
-              logger.e('${snapshot.data()?['questionAndAnswer']}');
+              // logger.e('Que and Ans $questionAndAnswer');
+              logger.e(questionAndAnswer?[0]['questionsAndAnswers']['answers']
+                  .length);
+              // logger.e('${snapshot.data()?['questionAndAnswer']}');
             });
             //  startTimer();
           } else {
@@ -79,7 +81,7 @@ class _QuizAnswerScreenState extends State<QuizAnswerScreen> {
       if (snapshot.exists) {
         setState(() {
           questionAndAnswer = snapshot.data()!['questionAndAnswer'];
-          logger.e('que $questionAndAnswer');
+          // logger.e('que $questionAndAnswer');
         });
         startTimer();
       } else {
@@ -107,91 +109,30 @@ class _QuizAnswerScreenState extends State<QuizAnswerScreen> {
                     ),
                   ),
                 ),
+
+                ListView.builder(
+                  scrollDirection: Axis.vertical,
+                    itemCount: questionAndAnswer.length,
+                    itemBuilder: (context, index) {
+                      Text(
+                        'Q. ${questionAndAnswer?[index]['questionsAndAnswers']['question']}',
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      );
+                      return null;
+                    })
+
                 // ListTile(
                 //   title: Text(
-                //     'Q. ${questionAndAnswer?['question'] ?? 'N/A'}',
+                //     'Ans. ${questionAndAnswer?[0]['questionsAndAnswers']['answers'][0]['answer']}',
                 //     style: const TextStyle(
                 //       fontSize: 18,
                 //       fontWeight: FontWeight.bold,
                 //     ),
                 //   ),
                 // ),
-                if (questionAndAnswer.contains('answers') &&
-                    questionAndAnswer['answers'] is List<dynamic>)
-                  Column(
-                    children: [
-                      for (var answerIndex = 0;
-                          answerIndex < questionAndAnswer['answers'].length;
-                          answerIndex++)
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            left: 10,
-                            bottom: 5,
-                          ),
-                          child: Row(
-                            children: [
-                              Text(
-                                '${answerIndex + 1}. ',
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Expanded(
-                                child: RadioListTile(
-                                  key:
-                                      Key('$answerIndex$_currentQuestionIndex'),
-                                  title: Text(
-                                    questionAndAnswer['answers'][answerIndex]
-                                            ['answer']
-                                        .toString(),
-                                    style: TextStyle(
-                                      color: groupValues.length > 0 &&
-                                              groupValues[0] ==
-                                                  '$answerIndex$_currentQuestionIndex'
-                                          ? Colors.blue
-                                          : Colors.black,
-                                    ),
-                                  ),
-                                  activeColor: groupValues.length > 0 &&
-                                          groupValues[0] ==
-                                              '$answerIndex$_currentQuestionIndex'
-                                      ? Colors.blue
-                                      : Colors.grey,
-                                  value: '$answerIndex$_currentQuestionIndex',
-                                  groupValue: groupValues.length > 0
-                                      ? groupValues[0]
-                                      : null,
-                                  onChanged: (selectedValue) {
-                                    setState(() {
-                                      groupValues = [selectedValue!];
-                                    });
-                                  },
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                    ],
-                  ),
-                const Divider(
-                  height: 1,
-                  color: Colors.black12,
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                      child: const Text('Next Question'),
-                      onPressed: () {
-                        _timer.cancel();
-                        _timerDuration = 30;
-                        _currentQuestionIndex++;
-                        _loadNextQuestion();
-                      },
-                    ),
-                  ),
-                ),
               ],
             )
           : const Center(
