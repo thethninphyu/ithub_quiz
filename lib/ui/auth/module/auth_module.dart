37b65799683675ecd398aa172fdabc41e9d6075b
Thet Hnin <thethnin@momnpop.asia>
2024-03-13 19:15:25 +0630

    [add auth module]

--- lib/ui/auth/module/auth_module.dart
+++ lib/ui/auth/module/auth_module.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:ithub_quiz/ui/admin_screen/home_screen.dart';
 import 'package:ithub_quiz/ui/app_routes.dart';
 import 'package:ithub_quiz/ui/auth/login_widget.dart';
 import 'package:ithub_quiz/ui/auth/module/auth_routes.dart';
@@ -12,7 +13,9 @@ class AuthModule extends Module {
   @override
   List<ModularRoute> get routes => [
         ChildRoute(AuthRoutes.root,
-            child: (context, args) => const SplashScreen()),
+            child: (context, args) => const HomeScreen(
+                  status: 'home',
+                )),
         ChildRoute(AppRoutes.login,
             child: ((context, args) => const LoginWidget())),
         ChildRoute(AppRoutes.register,
